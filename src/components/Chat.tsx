b6408959122aa33baec05617f5b0590c61f2be49
its-shivam1008 <tanil5960@gmail.com>
2025-03-07 11:20:02 +0530

    add bg image and footer colors

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -3,9 +3,11 @@ import { IoSend } from 'react-icons/io5'
 
 const Chat = () => {
   return (
-    <div className="h-full flex flex-col">
-      <div className='bg-[#fffafa] flex-grow'> messagee display </div>
-      <div className="h-[20%] flex items-center"> 
+    <div className="m-2 rounded-[8px] h-full flex flex-col bg-[url('https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png')]">
+      <div className='bg-transparent flex-grow'> 
+      
+      </div>
+      <div className="h-[20%] flex items-center bg-transparent"> 
         <div className="messagebox w-[95%] flex items-center mx-auto ">
           <input type="text" name="message" id="message" className='w-[85%] px-1 py-2 rounded-[6px] border-[#5cf25c] border-1 outline-[#5cf25c] bg-[#fffafa] mx-2' />
           <button type="button" className='bg-[#5cf25c] p-2 rounded-full cursor-pointer'><IoSend size={20} /></button>
